5a665df6a94fe1b7f190f41aa43a517eb0bd0ca9
Br0deNN <theo.broden@gmail.com>
2023-10-20 21:09:20 +0200

    Add project files.

---
+++ Uppgift 4.8 inlämning/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace inlämning_4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en sträng");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("Skriv in två tecken som du vill leta efter");
+            string t = Console.ReadLine();
+
+            char t1 = t[0];
+            char t2 = t[1];
+            bool teckenfinns = false;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i]== t1 || s[i] == t2)
+                {
+                    teckenfinns |= true;
+                }
+            }
+            if (teckenfinns)
+            {
+                Console.WriteLine("Teckerna du letar efter finns!");
+            }
+            else
+            {
+                Console.WriteLine("Teckerna du letar efter finns ej!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
